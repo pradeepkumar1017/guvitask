f01908d0b2cd48a30fd13fb619bc5a3effa8d9ef
pradeepkumar1017 <60417284+pradeepkumar1017@users.noreply.github.com>
2020-01-29 12:09:51 +0530

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,18 +1,18 @@
-import random
 
-strs = ['1','2','3','4','5']
+import random
+strs = ['A','B','C','D','E','F']
 count = 0
 string = random.choice(strs)
 
 for i in range(5):
-    c = input('Enter a number: ')
+    c = input('Enter a character: ')
     if c in string and c!='':
         print('Good')
         counts = count + 1
         if count == 3:
             break
     else:
-        print('You entered a wrong number')
+        print('You entered a wrong character')
         
 if count == 3:
     print('Won')
